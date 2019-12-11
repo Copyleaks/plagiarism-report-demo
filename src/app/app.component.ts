1ba2ad46ce0529a2888f57ee0fc30e0183a35378
tal-copyleaks <tala@copyleaks.com>
2019-12-11 18:45:32 +0200

    add demo with angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,45 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import {
+  CopyleaksService,
+  CompleteResult,
+  ScanResult,
+  ScannedDocument,
+  ScanSource
+} from "@copyleaks/plagiarism-report";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
+    <cr-copyleaks-report> </cr-copyleaks-report>
   `,
   styles: []
 })
 export class AppComponent {
-  title = 'plagiarism-report-demo';
+  constructor(private service: CopyleaksService, private http: HttpClient) {
+    this.http
+      .get<ScanSource>("/assets/example-scan/scan-source.json")
+      .subscribe(source => service.pushDownloadedSource(source));
+    this.http
+      .get<CompleteResult>("/assets/example-scan/complete-result.json")
+      .subscribe(completeResult => {
+        service.pushCompletedResult(completeResult);
+        for (const result of completeResult.results.batch) {
+          this.http
+            .get<ScanResult>(`/assets/example-scan/results/${result.id}.json`)
+            .subscribe(scanResult => service.pushScanResult(result.id, scanResult));
+        }
+        for (const result of completeResult.results.internet) {
+          this.http
+            .get<ScanResult>(`/assets/example-scan/results/${result.id}.json`)
+            .subscribe(scanResult => service.pushScanResult(result.id, scanResult));
+        }
+        for (const result of completeResult.results.internet) {
+          this.http
+            .get<ScanResult>(`/assets/example-scan/results/${result.id}.json`)
+            .subscribe(scanResult => service.pushScanResult(result.id, scanResult));
+        }
+      });
+  }
+  title = "plagiarism-report-demo";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
+import { CopyleaksReportModule } from "@copyleaks/plagiarism-report";
+import { CustomMaterialModule } from "./material.module";
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    HttpClientModule,
+    CustomMaterialModule,
+    CopyleaksReportModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
